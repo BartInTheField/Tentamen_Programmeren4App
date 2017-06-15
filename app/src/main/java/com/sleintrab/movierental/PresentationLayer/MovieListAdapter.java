aafdc7adbc01f8b74d106a85d2d10dd755eec378
Bart in 't Veld <bart.int.007@gmail.com>
2017-06-15 22:42:37 +0200

    Fastscrolling enabled with help of sections

--- app/src/main/java/com/sleintrab/movierental/PresentationLayer/MovieListAdapter.java
+++ app/src/main/java/com/sleintrab/movierental/PresentationLayer/MovieListAdapter.java
@@ -5,22 +5,84 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.SectionIndexer;
 import android.widget.TextView;
 import com.sleintrab.movierental.R;
 
 import com.sleintrab.movierental.DomainModel.Movie;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Niels on 6/15/2017.
  */
 
-public class MovieListAdapter extends ArrayAdapter<Movie> {
+public class MovieListAdapter extends ArrayAdapter<Movie> implements
+        SectionIndexer {
+
+    HashMap<String, Integer> sectionsMap = new HashMap<String, Integer>();
+    ArrayList<String> sectionsList = new ArrayList<String>();
+
+    ArrayList<Integer> sectionForPosition = new ArrayList<Integer>();
+    ArrayList<Integer> positionForSection = new ArrayList<Integer>();
 
 
     public MovieListAdapter(Context context, ArrayList<Movie> movieList){
         super(context, R.layout.movie_list_item, movieList);
+
+
+        // Figure out what the sections should be (one section per unique
+        // initial letter, to accommodate letters that might be missing,
+        // or characters like ,)
+        for (int i = 0; i < movieList.size(); i++) {
+            String objectString = movieList.get(i).getTitle();
+            if (objectString.length() > 0) {
+                String firstLetter = objectString.substring(0, 1).toUpperCase();
+                if (!sectionsMap.containsKey(firstLetter)) {
+                    sectionsMap.put(firstLetter, sectionsMap.size());
+                    sectionsList.add(firstLetter);
+                }
+            }
+        }
+
+        // Calculate the section for each position in the list.
+        for (int i = 0; i < movieList.size(); i++) {
+            String objectString = movieList.get(i).getTitle();
+            if (objectString.length() > 0) {
+                String firstLetter = objectString.substring(0, 1).toUpperCase();
+                if (sectionsMap.containsKey(firstLetter)) {
+                    sectionForPosition.add(sectionsMap.get(firstLetter));
+                } else
+                    sectionForPosition.add(0);
+            } else
+                sectionForPosition.add(0);
+        }
+
+        // Calculate the first position where each section begins.
+        for (int i = 0; i < sectionsMap.size(); i++)
+            positionForSection.add(0);
+        for (int i = 0; i < sectionsMap.size(); i++) {
+            for (int j = 0; j < movieList.size(); j++) {
+                Integer section = sectionForPosition.get(j);
+                if (section == i) {
+                    positionForSection.set(i, j);
+                    break;
+                }
+            }
+        }
+    }
+
+    public int getPositionForSection(int section) {
+        return positionForSection.get(section);
+    }
+
+    public int getSectionForPosition(int position) {
+        return sectionForPosition.get(position);
+    }
+
+    public Object[] getSections() {
+        return sectionsList.toArray();
     }
 
     @Override
