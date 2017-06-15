80525c20fdfebae47f19d7e3f26a8a584b20e123
Niels van Dam <nielsvdam@hotmail.com>
2017-06-15 12:22:34 +0200

    Refactored the activity classes to PresentationLayer. Added HireFragment and edited MainActivity to extend from FragmentActivity

---
+++ app/src/main/java/com/sleintrab/movierental/PresentationLayer/HireFragment.java
@@ -0,0 +1,10 @@
+package com.sleintrab.movierental.PresentationLayer;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Niels on 6/15/2017.
+ */
+
+public class HireFragment extends Fragment {
+}
