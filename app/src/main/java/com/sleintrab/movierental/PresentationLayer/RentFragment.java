7c8cd4e190f9f9bf0bb4b8183fc5de9582cb380d
nielss94 <nielsvdam@hotmail.com>
2017-06-15 21:21:11 +0200

    MovieAPI connector and rentfragment with callback.

--- app/src/main/java/com/sleintrab/movierental/PresentationLayer/RentFragment.java
+++ app/src/main/java/com/sleintrab/movierental/PresentationLayer/RentFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.sleintrab.movierental.API.MovieAPI;
 import com.sleintrab.movierental.DomainModel.Movie;
 import com.sleintrab.movierental.R;
 
@@ -17,10 +18,12 @@ import java.util.ArrayList;
  * Created by Niels on 6/15/2017.
  */
 
-public class RentFragment extends Fragment {
+public class RentFragment extends Fragment implements MovieAPI.OnMoviesAvailable {
 
     private ListView movieListView;
     private MovieListAdapter movieListAdapter;
+    private MovieAPI movieAPI;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
@@ -31,19 +34,12 @@ public class RentFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        ArrayList<Movie> movies = new ArrayList<>();
-        movies.add(new Movie(1, "nop", "nwwwe", 1994, 8, 2.99, 120, 29.99, "GDK", "Felix die op een wip zit"));
-        movies.add(new Movie(1, "nop", "nwwwe", 1994, 8, 2.99, 120, 29.99, "GDK", "Felix die op een wip zit"));
-        movies.add(new Movie(1, "nop", "nwwwe", 1994, 8, 2.99, 120, 29.99, "GDK", "Felix die op een wip zit"));
-        movies.add(new Movie(1, "nop", "nwwwe", 1994, 8, 2.99, 120, 29.99, "GDK", "Felix die op een wip zit"));
-        movies.add(new Movie(1, "nop", "nwwwe", 1994, 8, 2.99, 120, 29.99, "GDK", "Felix die op een wip zit"));
-        movies.add(new Movie(1, "nop", "nwwwe", 1994, 8, 2.99, 120, 29.99, "GDK", "Felix die op een wip zit"));
-        movies.add(new Movie(1, "nop", "nwwwe", 1994, 8, 2.99, 120, 29.99, "GDK", "Felix die op een wip zit"));
-        movies.add(new Movie(1, "nop", "nwwwe", 1994, 8, 2.99, 120, 29.99, "GDK", "Felix die op een wip zit"));
-        movies.add(new Movie(1, "nop", "nwwwe", 1994, 8, 2.99, 120, 29.99, "GDK", "Felix die op een wip zit"));
-        movies.add(new Movie(1, "nop", "nwwwe", 1994, 8, 2.99, 120, 29.99, "GDK", "Felix die op een wip zit"));
-        movies.add(new Movie(1, "nop", "nwwwe", 1994, 8, 2.99, 120, 29.99, "GDK", "Felix die op een wip zit"));
+        movieAPI = new MovieAPI(getActivity().getApplicationContext(), this);
         movieListView = (ListView)getView().findViewById(R.id.rent_movie_listView);
+    }
+
+    @Override
+    public void onMoviesAvailable(ArrayList<Movie> movies) {
         movieListAdapter = new MovieListAdapter(getActivity().getApplicationContext(), movies);
         movieListView.setAdapter(movieListAdapter);
     }
