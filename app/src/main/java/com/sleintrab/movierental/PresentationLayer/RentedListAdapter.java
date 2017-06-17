09456559f6a48a7216ed911bae27b26f17b88fc7
Bart in 't Veld <bart.int.007@gmail.com>
2017-06-17 19:12:12 +0200

    Hand in now is return + added return in .. days

--- app/src/main/java/com/sleintrab/movierental/PresentationLayer/RentedListAdapter.java
+++ app/src/main/java/com/sleintrab/movierental/PresentationLayer/RentedListAdapter.java
@@ -1,6 +1,7 @@
 package com.sleintrab.movierental.PresentationLayer;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,14 @@ import com.sleintrab.movierental.DomainModel.Movie;
 import com.sleintrab.movierental.DomainModel.Rental;
 import com.sleintrab.movierental.R;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Niels on 6/15/2017.
@@ -41,8 +49,32 @@ public class RentedListAdapter extends ArrayAdapter<Rental> {
         movieTitle.setText(rental.getMovie().getTitle());
         movieYear.setText(String.valueOf(rental.getMovie().getReleaseYear()));
         movieRating.setText(rental.getMovie().getRating());
-        rentalReturn.setText(rental.getReturnDate());
+
+        int days = daysTillReturn(rental);
+        if (days <= 0){
+            rentalReturn.setText("Return today");
+        } else {
+            rentalReturn.setText("Return in " + days + " days");
+        }
 
         return customView;
+    }
+
+    private int daysTillReturn(Rental rental){
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        int days = 0;
+        try {
+            Date date = df.parse(rental.getReturnDate().substring(0, 10));
+            days = daysBetween(new Date(), date);
+            return days;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return days;
+    }
+
+    private int daysBetween(Date d1, Date d2){
+        return (int)( (d2.getTime() - d1.getTime()) / (1000 * 60 * 60 * 24));
     }
 }
